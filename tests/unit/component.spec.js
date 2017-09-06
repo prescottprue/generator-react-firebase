e1a78867c2fa2429b65e5c0a60097bae6b300bfc
Scott Prue <scott@residenetwork.com>
2017-09-05 23:52:52 -0700

    Index file test added.

--- tests/unit/component.spec.js
+++ tests/unit/component.spec.js
@@ -4,24 +4,42 @@ import { checkForEachFile } from '../utils'
 
 const name = 'Test'
 const folderPath = `src/components/${name}`
+var testPath = path.join(__dirname, 'temp2');
+const createFilePath = (name) => path.join(__dirname, 'temp2', folderPath, name)
 
 describe('generator-react-firebase:component', () => {
-  before(() =>
+  beforeEach(function (done) {
+    var self = this
     helpers.run(path.join(__dirname, '../../generators/component'))
       .withArguments([ name ])
-      .withPrompts({includeStyles: true})
-      .toPromise()
-  )
+      .inDir(testPath)
+      .withPrompts({ includeStyles: true })
+      .on('end', done)
+  })
+
+  describe('index file', () => {
+    it('is copied', () => {
+      checkForEachFile([ createFilePath('index.js') ], testPath)
+    })
+    // TODO: Check that content of file is correct
+    // it('has correct content', () => {
+    //   assert.fileContent('app/components/${name}/${name}.js', //)
+    // })
+  })
 
   describe('js file', () => {
-    checkForEachFile([ `${folderPath}/${name}.js` ], folderPath)
+    it('is copied', () => {
+      return checkForEachFile([ createFilePath(`${name}.js`)  ], testPath)
+    })
     // TODO: Check that content of file is correct
     // it('has correct content', () => {
     //   assert.fileContent('app/components/${name}/${name}.js', //)
     // })
   })
   describe('scss file', () => {
-    checkForEachFile([ `${folderPath}/${name}.scss` ], folderPath)
+    it('is copied', () => {
+      return checkForEachFile([ createFilePath(`${name}.scss`) ], testPath)
+    })
     // TODO: Check that content of file is correct
     // it('has correct content', () => {
     //   assert.fileContent('app/components/Test/Test.js', //)
