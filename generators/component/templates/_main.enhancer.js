778102397ced013e71821869288d506275e3a317
qalqi <qalqi@qalqi.com>
2017-12-25 18:44:05 +0530

    Fixes missing firebaseConnect import
    When we are not using firestoreConnect during yo generate component, firebaseConnect should be imported. As of now, firebaseConnect is not imported causing an error. This change will fix it.

--- generators/component/templates/_main.enhancer.js
+++ generators/component/templates/_main.enhancer.js
@@ -1,6 +1,6 @@
 import { compose } from 'redux'
 import { connect } from 'react-redux'
-import { <% if (usingFirestore) { %>firestoreConnect<% } %><% if (!usingFirestore) { %>firestoreConnect<% } %> } from 'react-redux-firebase'
+import { <% if (usingFirestore) { %>firestoreConnect<% } %><% if (!usingFirestore) { %>firebaseConnect<% } %> } from 'react-redux-firebase'
 
 export default compose(
   // create listener for <%= lowerName %>, results go into redux
