5d9dabc75be3d60d364d3dc0ecb20bb120c8005c
Scott Prue <scott@residenetwork.com>
2018-01-11 14:31:41 -0800

    fix project page enhancer - #82

--- generators/app/templates/src/routes/Projects/routes/Project/components/ProjectPage/ProjectPage.enhancer.js
+++ generators/app/templates/src/routes/Projects/routes/Project/components/ProjectPage/ProjectPage.enhancer.js
@@ -1,13 +1,27 @@
 import { compose } from 'redux'
 import { connect } from 'react-redux'
-import { firebaseConnect, getVal } from 'react-redux-firebase'
+import { firestoreConnect, getVal } from 'react-redux-firebase'
 import { spinnerWhileLoading } from 'utils/components'
+import { UserIsAuthenticated } from 'utils/router'
 
 export default compose(
-  // Create listener for project
-  firebaseConnect(({ params }) => [{ path: `projects/${params.projectname}` } ]),
-  // Map project from redux state to props
-  connect(({ firebase: { data } }, { params }) => ({
+  // redirect to /login if user is not logged in
+  UserIsAuthenticated,
+  // Map auth uid from state to props
+  connect(({ firebase: { auth: { uid } } }) => ({ uid })),
+  // Wait for uid to exist before going further
+  spinnerWhileLoading(['uid']),
+  // Create listeners based on current users UID
+  firestoreConnect(({ params, uid }) => [
+    // Listener for projects the current user created
+    {
+      collection: 'projects',
+      where: ['createdBy', '==', uid]
+    }
+  ]),
+  spinnerWhileLoading(['firestore']),
+  // // Map projects from state to props
+  connect(({ firestore: { data } }, {params}) => ({
     project: getVal(data, `projects/${params.projectname}`)
   })),
   // Show loading spinner while project is loading
