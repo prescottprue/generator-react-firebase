ad379e15c2248306b0529578e726b7cbd96197bc
Scott Prue <scott@residebrokerage.com>
2018-04-20 00:28:06 -0700

    fix(formGenerator): add missing import in enhancer - #98
    * feat(app): firebase.json now includes functions predeploy script

--- generators/form/templates/_main.enhancer.js
+++ generators/form/templates/_main.enhancer.js
@@ -1,6 +1,7 @@
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { withHandlers } from 'recompose'
+import { reduxForm } from 'redux-form'
 
 export default compose(
   // Add form capabilities (including submit and validation handling)
