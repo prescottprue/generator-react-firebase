e760f306b5d2a834dfddcebc8d3a144987dd3a81
prescottprue <sprue.dev@gmail.com>
2018-10-25 17:42:40 -0700

    Readme cleanup (formatting in FAQ)

--- generators/function/templates/_rtdbFunction.js
+++ generators/function/templates/_rtdbFunction.js
@@ -3,13 +3,13 @@ import * as admin from 'firebase-admin'<% if (airbnbLinting) { %>;<% } %>
 import { to } from 'utils/async'<% if (airbnbLinting) { %>;<% } %>
 
 /**
- * <% if (!functionsV1) { %>@param  {functions.Event} event - Function event<% } %><% if (functionsV1 && (eventType === 'onWrite' || eventType === 'onUpdate')) { %>@param  {functions.Change} change - Functions change
- * @param {functions.database.DataSnapshot} change.before - Snapshot from before change event
+ * <% if (!functionsV1) { %>@param  {functions.Event} event - Function event<% } %><% if (functionsV1 && (eventType === 'onWrite' || eventType === 'onUpdate')) { %>@param  {functions.Change} change - Function change interface containing state objects
+ * @param {functions.database.DataSnapshot} change.before - State prior to the event.
  * @param {Function} change.before.val - Value before change event
- * @param {functions.database.DataSnapshot} change.after - Snapshot from after change event
+ * @param {functions.database.DataSnapshot} change.after - State after the event.
  * @param {Function} change.after.val - Value after change event<% } else {%>@param  {functions.database.DataSnapshot} snap - Data snapshot of the event
- * @param {Function} snap.val - Value after change event<% } %><% if (functionsV1) { %>
- * @param {functions.EventContext} context - Functions context
+ * @param {Function} snap.val - Value after event<% } %><% if (functionsV1) { %>
+ * @param {functions.EventContext} context - Function event context
  * @param {Object} context.auth - Authentication information for the user that triggered the function<% } %>
  * @return {Promise}
  */
@@ -17,28 +17,39 @@ import { to } from 'utils/async'<% if (airbnbLinting) { %>;<% } %>
   // const { params, auth, timestamp } = context
   const { before, after } = change
   console.log('<%= camelName %> <%= eventType %> event:', { before: before.val(), after: after.val() })
+  // Create RTDB for response
   const ref = admin.database().ref(`responses/<%= camelName %>`).push()<% if (airbnbLinting) { %>;<% } %>
-  const [writeErr, response] = await to(ref.set({ hello: 'world' }))<% if (airbnbLinting) { %>;<% } %>
+  // Write data to RTDB
+  const [writeErr] = await to(ref.set({ hello: 'world' }))<% if (airbnbLinting) { %>;<% } %>
+  // Handle errors writing data to RTDB
   if (writeErr) {
     console.error(`Error writing response: ${writeErr.message || ''}`, writeErr)<% if (airbnbLinting) { %>;<% } %>
     throw writeErr<% if (airbnbLinting) { %>;<% } %>
   }
-  return response<% if (airbnbLinting) { %>;<% } %>
+  // End function execution by returning
+  return null<% if (airbnbLinting) { %>;<% } %>
 }<% } else if (functionsV1 && eventType !== 'onWrite' && eventType !== 'onUpdate') { %>async function <%= camelName %>Event(snap, context) {
   const { params: { pushId } } = context
   console.log('<%= camelName %> <%= eventType %> event:', snap.val())
+  // Create RTDB for response
   const ref = admin.database().ref(`responses/<%= camelName %>/${pushId}`)<% if (airbnbLinting) { %>;<% } %>
-  const [writeErr, response] = await to(ref.set({ hello: 'world' }))<% if (airbnbLinting) { %>;<% } %>
+  // Write data to RTDB
+  const [writeErr] = await to(ref.set({ hello: 'world' }))<% if (airbnbLinting) { %>;<% } %>
+  // Handle errors writing data to RTDB
   if (writeErr) {
     console.error(`Error writing response: ${writeErr.message || ''}`, writeErr)<% if (airbnbLinting) { %>;<% } %>
     throw writeErr<% if (airbnbLinting) { %>;<% } %>
   }
-  return response<% if (airbnbLinting) { %>;<% } %>
+  // End function execution by returning
+  return null<% if (airbnbLinting) { %>;<% } %>
 }<% } else { %>async function <%= camelName %>Event(event) {
   const { params: { pushId }, data } = event
   console.log('<%= camelName %> <%= eventType %> event:', event.val())
+  // Create RTDB for response
   const ref = admin.database().ref(`responses/<%= camelName %>/${pushId}`)<% if (airbnbLinting) { %>;<% } %>
-  const [writeErr, response] = await to(ref.set({ hello: 'world' }))<% if (airbnbLinting) { %>;<% } %>
+  // Write data to RTDB
+  const [writeErr] = await to(ref.set({ hello: 'world' }))<% if (airbnbLinting) { %>;<% } %>
+  // Handle errors writing data to RTDB
   if (writeErr) {
     console.error(
       `Error writing response: ${writeErr.message || ''}`,
@@ -46,7 +57,8 @@ import { to } from 'utils/async'<% if (airbnbLinting) { %>;<% } %>
     )<% if (airbnbLinting) { %>;<% } %>
     throw writeErr<% if (airbnbLinting) { %>;<% } %>
   }
-  return response<% if (airbnbLinting) { %>;<% } %>
+  // End function execution by returning
+  return null<% if (airbnbLinting) { %>;<% } %>
 }<% } %><% if (airbnbLinting) { %>;<% } %>
 
 /**
