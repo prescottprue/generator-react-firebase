8f48e90c40bc26da211a7c447a0f5c4041c82ddf
prescottprue <sprue.dev@gmail.com>
2019-01-24 16:44:56 -0800

    Component is named function

--- generators/component/templates/_main.js
+++ generators/component/templates/_main.js
@@ -2,12 +2,14 @@
 import PropTypes from 'prop-types'<% } else { %>import React, { PropTypes } from 'react'<% } %><% if (addStyle && styleType === 'scss') { %>
 import classes from './<%= name %>.scss'<%}%>
 
-export const <%= name %> = ({ <%= camelName %><% if (addStyle && styleType !== 'scss') { %>, classes<%}%> }) => (
-  <% if (addStyle) { %><div className={classes.root}><% } else { %><div className="<%= name %>"><%}%>
-    <span><%= name %> Component</span>
-    <pre>{JSON.stringify(<%= camelName %>, null, 2)}</pre>
-  </div>
-)
+function <%= name %>({ <%= camelName %><% if (addStyle && styleType !== 'scss') { %>, classes<%}%> }) {
+  return (
+    <% if (addStyle) { %><div className={classes.root}><% } else { %><div className="<%= name %>"><%}%>
+      <span><%= name %> Component</span>
+      <pre>{JSON.stringify(<%= camelName %>, null, 2)}</pre>
+    </div>
+  )
+}
 
 <%= name %>.propTypes = {
   <% if (addStyle && styleType !== 'scss') { %>classes: PropTypes.object, // from enhancer (withStyles)
