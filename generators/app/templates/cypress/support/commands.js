06369f864edc5eece6e907d99a6650e9b16ace63
Scott Prue <scott@residebrokerage.com>
2020-03-22 17:49:00 -0700

    chore(app): update cypress-firebase dev dependency to v1.0.0-beta

--- generators/app/templates/cypress/support/commands.js
+++ generators/app/templates/cypress/support/commands.js
@@ -5,21 +5,20 @@ import 'firebase/storage'
 import 'firebase/firestore'
 import { attachCustomCommands } from 'cypress-firebase'
 
-const projectId = Cypress.env('FIREBASE_PROJECT_ID')
+const projectId = Cypress.env('FB_projectId') || Cypress.env('FIREBASE_PROJECT_ID')
 const env = Cypress.env('env') || 'stage'
 const apiKey =
+  Cypress.env('FB_apiKey') ||
   Cypress.env(`${env.toUpperCase()}_FIREBASE_API_KEY`) ||
   Cypress.env('FIREBASE_API_KEY')
 
 const fbConfig = {
   apiKey,
   authDomain: `${projectId}.firebaseapp.com`,
-  databaseURL: `https://${projectId}.firebaseio.com`,
+  databaseURL: Cypress.env('FB_databaseURL') || `https://${projectId}.firebaseio.com`,
   projectId: `${projectId}`,
   storageBucket: `${projectId}.appspot.com`
 }
-
-window.fbInstance = firebase.initializeApp(fbConfig)
 
 // Custom commands including login, signup, callRtdb, and callFirestore
 attachCustomCommands({ Cypress, cy, firebase })
