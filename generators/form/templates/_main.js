f17081d0870211bd7635d3164824646608aa1ceb
Scott Prue <scott@residebrokerage.com>
2020-03-23 21:22:32 -0700

    Fix typo in form template to fix test

--- generators/form/templates/_main.js
+++ generators/form/templates/_main.js
@@ -43,7 +43,7 @@ function <%= name %>({ onSubmit }) {
         disabled={isSubmitting || !isValid}>
         {isSubmitting ? 'Saving...' : 'Save'}
       </Button><% } %>
-    </form<%>
+    </form>
   )
 }
 
